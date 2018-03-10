328678a797f85c144cfd55cbbc6a27fbe5a01eb5
Cole Howard <uglyboxer@gmail.com>
2018-03-10 08:19:54 -0800

    Bigger model

--- train.py
+++ train.py
@@ -113,16 +113,16 @@ def create_sequences(tokenizer, max_length, descriptions, photos):
 def define_model(vocab_size, max_length):
     # feature extractor model
     inputs1 = Input(shape=(4096,))
-    fe1 = Dropout(0.5)(inputs1)
-    fe2 = Dense(256, activation='relu')(fe1)
+    fe1 = Dropout(0.2)(inputs1)
+    fe2 = Dense(512, activation='relu')(fe1)
     # sequence model
     inputs2 = Input(shape=(max_length,))
     se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
-    se2 = Dropout(0.5)(se1)
-    se3 = LSTM(256)(se2)
+    se2 = Dropout(0.2)(se1)
+    se3 = LSTM(1024)(se2)
     # decoder model
     decoder1 = add([fe2, se3])
-    decoder2 = Dense(256, activation='relu')(decoder1)
+    decoder2 = Dense(1024, activation='relu')(decoder1)
     outputs = Dense(vocab_size, activation='softmax')(decoder2)
     # tie it together [image, seq] [word]
     model = Model(inputs=[inputs1, inputs2], outputs=outputs)
