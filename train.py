1d46d4ab407540797e17f8168170915b8e267fc3
Cole Howard <uglyboxer@gmail.com>
2018-03-14 18:41:47 -0700

    fix

--- train.py
+++ train.py
@@ -122,26 +122,26 @@ def sequence_generator(tokenizer, max_length, descriptions, photos, vocab_size, 
     while True:
         X1, X2, y = list(), list(), list()
         # walk through each image identifier
-            for key, desc_list in descriptions.items():
-                # walk through each description for the image
-                for desc in desc_list:
-                    # encode the sequence
-                    seq = tokenizer.texts_to_sequences([desc])[0]
-                    # split one sequence into multiple X,y pairs
-                    for i in range(1, len(seq)):
-                        # split into input and output pair
-                        in_seq, out_seq = seq[:i], seq[i]
-                        # pad input sequence
-                        in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
-                        # encode output sequence
-                        out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
-                        # store
-                        X1.append(photos[key][0])
-                        X2.append(in_seq)
-                        y.append(out_seq)
-                        if len(X1) >= batch_size:
-                            yield [array(X1), array(X2)], array(y)
-                            X1, X2, y = list(), list(), list()
+        for key, desc_list in descriptions.items():
+            # walk through each description for the image
+            for desc in desc_list:
+                # encode the sequence
+                seq = tokenizer.texts_to_sequences([desc])[0]
+                # split one sequence into multiple X,y pairs
+                for i in range(1, len(seq)):
+                    # split into input and output pair
+                    in_seq, out_seq = seq[:i], seq[i]
+                    # pad input sequence
+                    in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+                    # encode output sequence
+                    out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+                    # store
+                    X1.append(photos[key][0])
+                    X2.append(in_seq)
+                    y.append(out_seq)
+                    if len(X1) >= batch_size:
+                        yield [array(X1), array(X2)], array(y)
+                        X1, X2, y = list(), list(), list()
 
 
 # define the captioning model
